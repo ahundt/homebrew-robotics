630dfd16256d7606ca9bdcec71d09ea53c2051e0
Andrew Hundt <ATHundt@gmail.com>
2015-08-11 18:46:15 -0400

    azmq initial implementation added

---
+++ azmq.rb
@@ -0,0 +1,34 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Azmq < Formula
+  desc "C++ language binding library integrating ZeroMQ with Boost Asio"
+  homepage "https://github.com/zeromq/azmq"
+  url "https://github.com:zeromq/azmq.git", :using => :git
+  sha256 ""
+
+  depends_on "cmake" => :build
+  depends_on "boost"
+  depends_on "zeromq"
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test azmq`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
