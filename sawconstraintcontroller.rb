5d05efafcf6106c5225779c2e15fe291e8f5a863
Andrew Hundt <ATHundt@gmail.com>
2015-08-25 21:58:39 -0400

    re-add sawconstraintcontroller

---
+++ sawconstraintcontroller.rb
@@ -0,0 +1,37 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Sawconstraintcontroller < Formula
+  desc "sawConstraintController is a cisst saw component that performs constrained optimization to plan inverse kinematics for a robot arm"
+  homepage "https://github.com/jhu-saw/sawConstraintController"
+  url "https://github.com/jhu-saw/sawConstraintController.git", :tag => "1.0.1"
+  head "https://github.com/jhu-saw/sawConstraintController.git", :branch => "devel"
+  version "1.0.1"
+  #sha256 ""
+
+  depends_on "cmake" => :build
+  depends_on "cisst" # may currently actually depend on cisst devel/HEAD version
+  depends_on "cisstnetlib"
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    args = std_cmake_args
+    
+    system "cmake", ".", *args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test azmq`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
