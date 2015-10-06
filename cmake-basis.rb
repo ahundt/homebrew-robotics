1b26619df3167c7d5b3bbfeb5260b32924702e84
Andrew Hundt <ATHundt@gmail.com>
2015-10-06 19:33:06 -0400

    cmake-basis.rb move cmake config to inside install step

--- cmake-basis.rb
+++ cmake-basis.rb
@@ -31,46 +31,47 @@ class CmakeBasis < Formula
     url "https://github.com/schuhschuh/cmake-basis.git", :using => :git, :branch => "master"
 
   end
-
-  cmake_args = std_cmake_args
-  cmake_args << "-DBUILD_DOCUMENTATION=OFF"
-  if build.with? "docs"
-    cmake_args << "-DUSE_Sphinx=ON"
-  else
-    cmake_args << "-DUSE_Sphinx=OFF"
-  end
-  if build.with? "perl"
-    cmake_args << "-DUSE_Perl=ON"
-  else
-    cmake_args << "-DUSE_Perl=OFF"
-  end
-  if build.with? "perl-utilities"
-    cmake_args << "-DBUILD_BASIS_UTILITIES_FOR_PERL=ON -DUSE_Perl=ON"
-  else
-    cmake_args << "-DBUILD_BASIS_UTILITIES_FOR_PERL=OFF"
-  end
-  if build.with? "python"
-    cmake_args << "-DUSE_PythonInterp=ON"
-  else
-    cmake_args << "-DUSE_PythonInterp=OFF"
-  end
-  if build.with? "python-utilities"
-    cmake_args << "-DBUILD_BASIS_UTILITIES_FOR_PYTHON=ON -DUSE_PythonInterp=ON"
-  else
-    cmake_args << "-DBUILD_BASIS_UTILITIES_FOR_PYTHON=OFF"
-  end
-  #if build.with? "jython"
-  #  cmake_args << "-DUSE_JythonInterp=ON"
-  #else
-    cmake_args << "-DUSE_JythonInterp=OFF"
-    #end
-  if build.with? "project-tool"
-    cmake_args << "-DBUILD_PROJECT_TOOL=ON"
-  else
-    cmake_args << "-DBUILD_PROJECT_TOOL=OFF"
-  end
-
+  
   def install
+
+    cmake_args = std_cmake_args
+    cmake_args << "-DBUILD_DOCUMENTATION=OFF"
+    if build.with? "docs"
+      cmake_args << "-DUSE_Sphinx=ON"
+    else
+      cmake_args << "-DUSE_Sphinx=OFF"
+    end
+    if build.with? "perl"
+      cmake_args << "-DUSE_Perl=ON"
+    else
+      cmake_args << "-DUSE_Perl=OFF"
+    end
+    if build.with? "perl-utilities"
+      cmake_args << "-DBUILD_BASIS_UTILITIES_FOR_PERL=ON -DUSE_Perl=ON"
+    else
+      cmake_args << "-DBUILD_BASIS_UTILITIES_FOR_PERL=OFF"
+    end
+    if build.with? "python"
+      cmake_args << "-DUSE_PythonInterp=ON"
+    else
+      cmake_args << "-DUSE_PythonInterp=OFF"
+    end
+    if build.with? "python-utilities"
+      cmake_args << "-DBUILD_BASIS_UTILITIES_FOR_PYTHON=ON -DUSE_PythonInterp=ON"
+    else
+      cmake_args << "-DBUILD_BASIS_UTILITIES_FOR_PYTHON=OFF"
+    end
+    #if build.with? "jython"
+    #  cmake_args << "-DUSE_JythonInterp=ON"
+    #else
+      cmake_args << "-DUSE_JythonInterp=OFF"
+      #end
+    if build.with? "project-tool"
+      cmake_args << "-DBUILD_PROJECT_TOOL=ON"
+    else
+      cmake_args << "-DBUILD_PROJECT_TOOL=OFF"
+    end
+    
     mkdir "build" do
       system "cmake", "-G", "Unix Makefiles", "..", *cmake_args
       system "make"
