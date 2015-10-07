4ec27177f0206d97cb1b6fe48239ffaaceffecb7
Andrew Hundt <ATHundt@gmail.com>
2015-10-06 21:47:49 -0400

    cmake-basis.rb fixed homepage

--- cmake-basis.rb
+++ cmake-basis.rb
@@ -1,6 +1,6 @@
 class CmakeBasis < Formula
   desc "cmake build and installer generation tools"
-  homepage "https://github.com/schuhschuh/cmake-basis"
+  homepage "http://opensource.andreasschuh.com/cmake-basis/"
   url "http://schuhschuh.github.io/cmake-basis/", :using => :git, :tag => "v3.2.0"
   version "3.2"
   #sha256 ""
