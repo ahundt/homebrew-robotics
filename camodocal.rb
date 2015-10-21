de26a64975e129f82fe0399c97fe3797e9253c29
Andrew Hundt <ATHundt@gmail.com>
2015-10-21 15:04:26 -0400

    added initial camodocal.rb

---
+++ camodocal.rb
@@ -0,0 +1,42 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Camodocal < Formula
+  desc "CamOdoCal: Automatic Intrinsic and Extrinsic Calibration of a Rig with Multiple Generic Cameras and Odometry"
+  homepage "https://github.com/hengli/camodocal"
+  url "https://github.com/hengli/camodocal.git", :branch => "master"
+  head "https://github.com/hengli/camodocal.git", :branch => "devel"
+  version "1.0.1"
+  #sha256 ""
+
+  depends_on "cmake"        => :build
+  depends_on "suite-sparse" => :required
+  depends_on "eigen"        => :required
+  depends_on "opencv"       => :recommended
+  depends_on "opencv3"      => :optional
+  depends_on "gflags"       => :recommended
+  depends_on "glog"         => :recommended
+  depends_on "openblas"     => :recommended
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    args = std_cmake_args
+    
+    system "cmake", ".", *args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test azmq`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
