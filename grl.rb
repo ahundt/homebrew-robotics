f5764e2fa428b9d59a7f524e1864604e865a389c
Andrew Hundt <ATHundt@gmail.com>
2015-11-02 20:51:08 -0500

    grl.rb no longer do in-source cmake build

--- grl.rb
+++ grl.rb
@@ -62,9 +62,12 @@ class Grl < Formula
     if build.with? "cisstnetlib"
       cmake_args << "-DCisstNetlib_DIR=#{Formula["cisstnetlib"].opt_prefix}/cmake"
     end
-
-    system "cmake", ".", *cmake_args
-    system "make", "install"
+    
+    mkdir "build" do
+      system "cmake", "-G", "Unix Makefiles", "..", *cmake_args
+      system "make"
+      system "make", "install"
+   end
   end
 
   test do
