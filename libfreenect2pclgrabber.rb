fe9ec03a0f4e6f05817770c1d88a43886b210828
Nathan Smith <nathansforyou@gmail.com>
2015-11-04 19:51:11 -0500

    Added homebrew for kinect calibration and pclgrabber

---
+++ libfreenect2pclgrabber.rb
@@ -0,0 +1,20 @@
+class libfreenect2pclgrabber < Formula
+  desc "C++ language binding library integrating ZeroMQ with Boost Asio"
+  homepage "https://github.com/giacomodabisias/libfreenect2pclgrabber"
+  url  "https://github.com/giacomodabisias/libfreenect2pclgrabber.git", :branch => "master"
+  head "https://github.com/giacomodabisias/libfreenect2pclgrabber.git"
+  version "0.01"
+
+  depends_on "cmake" => :build
+  depends_on "pcl"
+  depends_on "opencv"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
