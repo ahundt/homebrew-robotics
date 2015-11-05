3b69744e5b106cca2fc3486bd1b21a17e843a453
Andrew Hundt <ATHundt@gmail.com>
2015-11-04 19:09:35 -0500

    sophus.rb added

---
+++ sophus.rb
@@ -0,0 +1,21 @@
+class Sophus < Formula
+  desc "C++ implementation of Lie Groups using Eigen."
+  homepage "https://github.com/arpg/Sophus"
+  url  "https://github.com/arpg/Sophus.git", :branch => "master"
+  head "https://github.com/arpg/Sophus.git"
+  version "0.9"
+
+  depends_on "cmake" => :build
+  depends_on "boost"
+  depends_on "zeromq"
+  depends_on "eigen"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
