8496664c483d572053d7c1c532a6cb0d5c879886
Andrew Hundt <ATHundt@gmail.com>
2015-12-01 01:30:41 -0500

    initial add of ur_modern_driver.rb

---
+++ ur_modern_driver.rb
@@ -0,0 +1,23 @@
+class UrModernDriver < Formula
+  desc "C++ language binding library integrating ZeroMQ with Boost Asio"
+  homepage "https://github.com/ThomasTimm/ur_modern_driver/"
+  url  "https://github.com/ahundt/ur_modern_driver.git", :branch => "master"
+  head "https://github.com/ahundt/ur_modern_driver.git"
+  version "0.01"
+
+  depends_on "cmake" => :build
+  depends_on "boost"
+
+  def install
+    cmake_args = std_cmake_args + %W[
+      -DBoost_DIR=#{Formula["boost"].opt_prefix}
+    ]
+    
+    system "cmake", ".", *cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
