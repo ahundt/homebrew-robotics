864e1dcc8e6ab999280b3f617088ab93de5b02fa
Andrew Hundt <ATHundt@gmail.com>
2015-12-05 15:12:39 -0500

    libpointmatcher.rb added

---
+++ libpointmatcher.rb
@@ -0,0 +1,26 @@
+class LibPointMatcher < Formula
+  desc "An Iterative Closest Point library for 2-D/3-D mapping in robotic
+"
+  homepage "https://github.com/ethz-asl/libpointmatcher"
+  url  "https://github.com/ethz-asl/libpointmatcher.git", :branch => "master"
+  head "https://github.com/ethz-asl/libpointmatcher.git"
+  version "1.2.1"
+
+  depends_on "cmake" => :build
+  depends_on "boost"
+  depends_on "eigen"
+  depends_on "libnabo"
+
+  def install
+    cmake_args = std_cmake_args + %W[
+      -DBoost_DIR=#{Formula["boost"].opt_prefix}
+    ]
+    
+    system "cmake", ".", *cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
