ecbd5343927b7a617c732d42244b7128eed178bd
Andrew Hundt <ATHundt@gmail.com>
2016-02-12 16:28:43 -0500

    trtk.rb added Transformation and Registration Toolkit for robotics and computer integrated surgery

---
+++ trtk.rb
@@ -0,0 +1,25 @@
+class Trtk < Formula
+  desc "Transformation and Registration Toolkit for robotics and computer integrated surgery"
+  homepage "https://github.com/RWTHmediTEC/TRTK"
+  url  "https://github.com/ahundt/TRTK.git", :branch => "master"
+  head "https://github.com/ahundt/TRTK.git"
+  version "0.01"
+
+  depends_on "cmake"        => :build
+  depends_on "flann"
+  depends_on "eigen"
+  depends_on "opencv"       => :recommended
+
+  def install
+    cmake_args = std_cmake_args + %W[
+      -DBoost_DIR=#{Formula["boost"].opt_prefix}
+    ]
+
+    system "cmake", ".", *cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
