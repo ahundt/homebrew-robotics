bf8ce430d209771cdc97815dbe96e44d9d2ab033
Andrew Hundt <ATHundt@gmail.com>
2016-05-17 13:41:12 -0400

    spacevecalg.rb added

---
+++ spacevecalg.rb
@@ -0,0 +1,26 @@
+class Spacevecalg < Formula
+  desc "Implementation of spatial vector algebra with the Eigen3 linear algebra library"
+  homepage "https://github.com/jorisv/SpaceVecAlg"
+  url  "https://github.com/jorisv/SpaceVecAlg.git" :branch => "default"
+  head "https://github.com/jorisv/SpaceVecAlg.git"
+  version "0.1"
+
+  depends_on "cmake" => :build
+  depends_on "eigen"
+  depends_on "boost"
+
+  def install
+    cmake_args = std_cmake_args + %W[
+    ]
+    
+    mkdir "build" do
+      system "cmake", "-G", "Unix Makefiles", "..", *cmake_args
+      system "make"
+      system "make", "install"
+   end
+  end
+
+  test do
+    system "false"
+  end
+end
